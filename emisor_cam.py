b18149bf28ca5f8e3f10bc1d05d88be3fab10b7a
atentohouston <122824700+atentohouston@users.noreply.github.com>
2025-08-27 09:17:07 -0600

    Create emisor_cam.py

---
+++ emisor_cam.py
@@ -0,0 +1,28 @@
+# Utilice protocolo TCP.
+import cv2
+import socket
+import struct
+
+SERVER_IP = "192.168.1.127" # Cambie según sus necesidades.
+PORT = 5555 # Cambie segund sus necesidades.
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER_IP, PORT))
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # Comprimir frame a JPEG
+    ret, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+    data = buffer.tobytes()
+    
+    # Enviar tamaño y datos
+    message_size = struct.pack(">L", len(data))  # ">L" = big-endian unsigned long
+    client_socket.sendall(message_size + data)
+
+cap.release()
+client_socket.close()
